ab801fb1050165d077e101beb281892296babde2
Conner Millard <cmillard10@ivytech.edu>
2026-05-04 16:52:57 -0400

    Update blog features (comments, signup, templates, UI fixes)

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 from django.utils import timezone
 
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(default=timezone.now)
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
